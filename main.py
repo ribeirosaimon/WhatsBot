b800d4ae46eda144ddbdfe7587179961b24fd345
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-07-02 14:38:20 -0300

    estudando sobre o projeto

--- main.py
+++ main.py
@@ -1,9 +1,65 @@
-from chatterbot import Chatbot
-from chatterbot.trainers import ListTrainer
-import win32com.client
-import speech_recognition as sr
-import os
+import nltk
+import numpy as np
+import tflearn
+import tensorflow
+import random
+import json
 
-bot = Chatbot('Urbe',read_only=True)
-bot.set_trainer(ListTrainer)
-bot.train(['opa!','Olá!','Beleza?','sim e vc?','Tambem estou bem!'])
+from nltk.stem.lancaste import LancasteStemmer
+
+stemmer = LancasteStemmer()
+
+with open('intents.json') as file:
+    data = json.load(file)
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent['tag'])
+
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
+
+words=[stemmer.stem(w.lower()) for w in words if w !='?']
+words=sorted(list(set(words)))
+
+labels=sorted(labels)
+training =[]
+output=[]
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds=[stemmer.stem(w) for w in doc]
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row=out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+    training.append(bag)
+    output.append(output_row)
+
+
+training = np.array(training)
+output = np.array(output)
+tensorflow.reset_default_graph()
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net,8)
+net = tflearn.fully_connected(net,8)
+net = tflearn.fully_connected(net, len(output[0]),activation='softmax')
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+model.fit(training, output, n_epoch=1000,batch_size=8,show_metrics=True)
+moodel.save('model.tflearn')

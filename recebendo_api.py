cfb2b9ee8de93395e6ea03c4565efa49c9316524
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-07-11 22:40:33 -0300

    commit

--- recebendo_api.py
+++ recebendo_api.py
@@ -1,29 +1,53 @@
-import bs4
 import requests
-from bs4 import BeautifulSoup
 import json
-from datetime import datetime
-from principal_stocks import *
-import urllib.parse
+import time
 
 
-def simular_site(acao):
-    with open('stock.json','w') as arquivo:
-        for x in principal_stocks:
-            try:
-                json_stock = find_stock(x)
-                arquivo.write(json_stock + '\n')
-                print(x)
-            except Exception as e:
-                pass
+
+def simular_site():
+    with open('stock.json','r') as json_file:
+        dados = json.load(json_file)
+        return dados
+
+
+lista = ['movi3','tcsa3','shul4']
 
 def get_api(stock):
-    api = 'https://secure-wildwood-34847.herokuapp.com/stock'
-    resposta = requests.request('GET', api + f'/{stock}')
-    acao = resposta.json()
-    adj_close = acao[f'{stock}']['adj_close']
-    vol = acao[f'{stock}']['vol']
-    high = acao[f'{stock}']['high']
-    print(avg_vol)
+    #stock = stock
+    #api = 'https://secure-wildwood-34847.herokuapp.com/stock'
+    #resposta = requests.request('GET', api + f'/{stock}')
+    #acao = resposta.json()
+    acao = simular_site()
+    dados_acao = acao[stock]
+    nome_acao = stock
+    adj_close = acao[stock]['adj_close']
+    avg_vol = acao[stock]['avg_vol']
+    vol = acao[stock]['vol']
+    high = acao[stock]['high']
+    low = acao[stock]['low']
+    return [nome_acao, adj_close, high, low, avg_vol, vol]
+
+def teste(*dados):
+    if dados[2] <= dados[1]:
+        print(f'Uhuul! {dados[0]} está na máxima do dia!')
+        return f'Uhuul! {dados[0]} está na máxima do dia!'
+    if dados[3] >= dados[1]:
+        print(f'A Cotação de {dados[0]} está na mínima do dia!')
+        return f'A Cotação de {dados[0]} está na mínima do dia!'
+
+    return print('Passou pelo teste UHUUUUUUUUUUUUUUUUU')
+
+
 if __name__ == '__main__':
-    get_api('movi3')
+    while True:
+        time.sleep(120)
+        a = get_api('shul4')
+        teste(*a)
+
+'''
+    while True:
+        for x in lista:
+            time.sleep(1)
+            a = get_api(x)
+            teste(*a)
+'''

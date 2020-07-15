9e0f59fafb36393f998ce5f3227186c7f9d07f2b
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-07-15 09:45:02 -0300

    commit

--- main.py
+++ main.py
@@ -3,22 +3,30 @@ from datetime import datetime
 from tratamentos.info_acao import verifica_acao
 from twilioBotWhat import mensagem_retorno
 
+numero_ralney = '554899628260'
+
 watch_list = [['movi3',15.00,17.00],['mypk3',12.00,14.54],['wege3',53.20,55.80]]
-numeros_cadastrados = ['554891784586','554899628260']
+numeros_cadastrados = ['554891784586','555596985884']
 
 if __name__ == '__main__':
+    hora_do_dia = int(datetime.now().strftime('%H'))
     try:
         while True:
-            for acao in watch_list:
-                try:
-                    retorno = verifica_acao(acao[0],acao[1],acao[2])
-                    time.sleep(5)
-                    for mensagem in retorno:
-                        if mensagem != None:
-                            mensagem_retorno(mensagem,*numeros_cadastrados)
-                except:
-                    print(f'Nao consegui verificar a ação {acao[0]}')
-            time.sleep(300)
+            print(f'Ainda não esta na hora de começar {hora_do_dia}')
+            while 10 <= hora_do_dia <= 17:
+                for acao in watch_list:
+                    try:
+                        retorno = verifica_acao(acao[0],acao[1],acao[2])
+                        time.sleep(5)
+                        for mensagem in retorno:
+                            if mensagem != None:
+                                mensagem_retorno(mensagem,*numeros_cadastrados)
+                    except:
+                        print(f'Nao consegui verificar a ação {acao[0]}')
+                time.sleep(300)
+                hora_do_dia = int(datetime.now().strftime('%H'))
+            print('dormindo por 15 minutos')
+            time.sleep(900)
 
 
     except Exception as e:

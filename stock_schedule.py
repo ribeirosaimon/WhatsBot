651c40a47ae8366d82a304f247c5928c4d8d0409
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-07-25 16:58:33 -0300

    Commit

--- stock_schedule.py
+++ stock_schedule.py
@@ -1,14 +1,21 @@
 import time
 from datetime import datetime
 from tratamentos.info_acao import verifica_acao
-from twilioBotWhat import mensagem_retorno
+from mensagemtwilio.twilioBotWhat import mensagem_retorno
+from arquivo_confidencial import *
 
-'''stock, stop_loss, stop_gain, entrada'''
+
+
 
 def schedule_conferencia():
-    watch_list = [['HGTX3',12.00, 16.13, 15.64],['MGLU3',77.00, 87.78, 85.04],]
-    numeros_cadastrados = ['554891784586',]
-    conferencia(watch_list, numeros_cadastrados)
+    ''' a WATCH_LIST é uma lista onde as ações que deve ser verificada nesse formato:
+    formato:['ACAO3',       10.00,        20.00,      11.00]
+    ou seja:  stock,      stop_loss,    stop_gain,   entrada
+        O NUMBERS é uma lista de todos os numeros que deve enviar mensagem no whats
+    formato: 55xxXXXXxxxx
+    ou seja: DDI, DDD e telefone de 8 digitos
+    '''
+    conferencia(WATCH_LIST, NUMBERS)
 
 def conferencia(watch_list, numeros_cadastrados, mensagem_conferencia = [], tempo_conferencia=600, contador = []):
 #    try:

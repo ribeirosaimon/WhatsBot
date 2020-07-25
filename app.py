dfbb9a4465b6d382c439b0566a9bef1fc7c0933d
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-07-25 15:55:43 -0300

    comit

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 import time
 import schedule
 from datetime import datetime
-from stock_schedule import conferencia
-
-watch_list = [['HGTX3',15.00, 16.13, 15.64],['MGLU3',84.00, 87.78, 85.04],['EZTC3',40.00, 41.09, 40.99], ['GRND3', 7.30, 8.27, 8.06],]
-numeros_cadastrados = ['554891784586','555596985884']
+from stock_schedule import schedule_conferencia
 
 
 if __name__ == '__main__':
-    try:
-        conferencia(watch_list, numeros_cadastrados)
-    except Exception as e:
-        print(e)
+    schedule.every(60).seconds.do(schedule_conferencia)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
--- stock_schedule.py
+++ stock_schedule.py
@@ -3,28 +3,33 @@ from datetime import datetime
 from tratamentos.info_acao import verifica_acao
 from twilioBotWhat import mensagem_retorno
 
+'''stock, stop_loss, stop_gain, entrada'''
 
+def schedule_conferencia():
+    watch_list = [['HGTX3',12.00, 16.13, 15.64],['MGLU3',77.00, 87.78, 85.04],]
+    numeros_cadastrados = ['554891784586',]
+    conferencia(watch_list, numeros_cadastrados)
 
-
-def conferencia(watch_list, numeros_cadastrados, tempo_conferencia=900):
-    contador = 0
-    mensagem_conferencia = []
-
+def conferencia(watch_list, numeros_cadastrados, mensagem_conferencia = [], tempo_conferencia=600, contador = []):
+#    try:
     for acao in watch_list:
+        #retorno da ação com as mensagens a enviar, coloquei esse sleep só pra não sobrecarregar
         retorno = verifica_acao(acao[0].lower(),acao[1],acao[2],acao[3])
         time.sleep(2)
         for mensagem in retorno:
             if mensagem != None:
+                #se tiver algo na mensagem vai abrir o arquivo em modo leitura pra conferir se não foi enviado nos ultimos 60 minutos
                 if mensagem not in mensagem_conferencia:
-                    #essa sms vai ser adicionada na lista de sms conferencia, para nao ser enviada sempre... somente no intervalo de 1 Hora
-                    mensagem_conferencia.append(mensagem)
+                    #se essa sms não tiver nas sms enviadas nos ultimos 60 minutos vamos fazer um arquivo com as novas mensagens
                     mensagem_retorno(mensagem, numeros_cadastrados)
+                    mensagem_conferencia.append(mensagem)
                     #contador de tempo vai zerar, caso a sms seja enviada
-                    contador = 0
+                    del(contador[:])
 
-
-    contador += tempo_conferencia
+#    except Exception as e:
+#        print('Ocorreu algum erro de : ', e)
+    contador.append(tempo_conferencia)
     time.sleep(tempo_conferencia)
-    if contador >= 1800:
+    if len(contador) >= 6:
         #apos 1 hora será limpa a lista das conferencia de mensagem
-        mensagem_conferencia = []
+        del(mensagem_conferencia[:])

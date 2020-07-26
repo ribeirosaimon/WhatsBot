0183f8782b9cc2b2b685552e9c1dbb46170c4853
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-07-26 11:32:08 -0300

    Create arquivo_telefone.py

---
+++ arquivo_telefone.py
@@ -0,0 +1,9 @@
+ACCONT_SIT = 'xxxxxxxxxxxxxxxxxxxxxxxx'
+AUTH_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+pessoa = '55xxXXXXxxxx'
+
+
+
+WATCH_LIST = [['ACAO3',12.00, 16.13, 15.64],['ACAO4',77.00, 87.78, 85.04],]
+NUMBERS = [pessoa,]

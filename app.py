9bc1a6f3c228442bcb1b540bcec378dd783d0b80
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-07-28 00:54:13 -0300

    Commit

--- app.py
+++ app.py
@@ -4,9 +4,15 @@ from datetime import datetime
 from stock_schedule import conferencia
 from arquivo_confidencial import WATCH_LIST, NUMBERS
 
+horario_comercial = int(datetime.now().strftime('%H'))
+
+
 if __name__ == '__main__':
-    schedule.every().wednesday.at('10:15').do(conferencia(watch_list=WATCH_LIST,
-        numeros_cadastrados=NUMBERS))   
+    schedule.every(1).to(20).minutes.do(conferencia, watch_list=WATCH_LIST,
+        numeros_cadastrados=NUMBERS)
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        if  10 <= horario_comercial <= 17:
+            schedule.run_pending()
+        else:
+            time.sleep(900)
+        time.sleep(15)
